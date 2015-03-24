7ad6a4eec448b6f4a63b5c329f1d853e8bf86ad1
hfiller <hfiller2@uwo.ca>
2015-03-23 20:21:37 -0400

    passport

--- server.js
+++ server.js
@@ -5,19 +5,16 @@ var bcrypt = require("bcrypt");
 var object = {
   client_id: "4098180171.4151359858"
 }
-var Slack = require('slack-node');
-
-apiToken = "4098180171.4151359858";
-
-slack = new Slack(apiToken);
-
-slack.api("users.list", function(err, response) {
-  console.log(response);
-});
-
-slack.api('chat.postMessage', {
-  text:'hello from nodejs',
-  channel:'#general'
-}, function(err, response){
-  console.log(response);
-});
+var Slack = require('passport-slack');
+var CLIENT_ID = "4098180171.4151359858";
+var CLIENT_SECRET = "7822857132be7254bff8078f2722d6c8"
+passport.use(new SlackStrategy({
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ SlackId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+))
